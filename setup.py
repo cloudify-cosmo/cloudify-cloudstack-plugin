bb0489bb1a2e9c7d8e1d8317fe117ff8ddb4a1de
Roeland Kuipers <rkuipers@schubergphilis.com>
2014-10-02 15:19:44 +0200

    AHHHJHJ

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 
     install_requires=[
         "cloudify-plugins-common",
-        "cloudify-plugins-common>=3.0"
+        "cloudify-plugins-common>=3.0",
         "apache-libcloud"
     ]
 )
