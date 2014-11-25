fd4e5f00420f1a78615177dea8cb411b91069765
limor-gs <limor@gigaspaces.com>
2014-11-25 22:21:43 +0200

    Bump version to 3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     zip_safe=True,
     name='cloudify-cloudstack-plugin',
-    version='3.1ga',
+    version='3.1',
     packages=[
         'cloudstack_plugin',
         'cloudstack_exoscale_plugin'
