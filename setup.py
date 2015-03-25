8535f91c229425c91d269bc8fd2409b6187a05d1
opencm <limor@gigaspaces.com>
2015-03-25 12:05:37 +0200

    Bump version to 1.2m8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     zip_safe=True,
     name='cloudify-cloudstack-plugin',
-    version='1.2a7',
+    version='1.2a8',
     packages=[
         'cloudstack_plugin',
         'cloudstack_exoscale_plugin'
@@ -14,7 +14,7 @@ setup(
 
     install_requires=[
         "cloudify-plugins-common",
-        "cloudify-plugins-common>=3.2a7",
+        "cloudify-plugins-common>=3.2a8",
         "apache-libcloud>=0.16"
     ]
 )
